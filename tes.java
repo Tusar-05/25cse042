46ebda15e3803e6c237bd3e135dc3703dc046523
Tusar Kanta Marei <tusarkantamarei@gmail.com>
2026-09-19 13:20:11 +0530

    Add files via upload

--- tes.java
+++ tes.java
@@ -1,7 +1,7 @@
 public class tes{
 	public static void main(String args[]){
 		String x= new String("abc");
-		Srting y= new String("abc");
+		String y= new String("abc");
 		System.out.println(x==y);
 		System.out.println(x.equals(y));
 	}
@@ -11,3 +11,11 @@ public class tes{
 // Which Strig is more in life 
 // what can be the result while == 
 // what can be the result equals 
+/*
+while string is generated using a string class the == operator return false because the == operator is not ment for conntent operation 
+rather it is for object comparison 
+
+-2 . Even in string buffer the == operator is meant for address comparison only 
+-3 . when we compare two objects with equals method for string class it returns true
+-4 . Whern you compare two objects using equals method of using string buffer class
+*/
